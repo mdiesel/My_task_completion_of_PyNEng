4b3f190075534b4e66254882408159eabef6a080
Yarl <yarlstel@gmail.com>
2017-02-23 12:29:39 +1100

    Добавил описание задач

--- 3/pyneng_3_3.py
+++ 3/pyneng_3_3.py
@@ -1,5 +1,8 @@
 '''
 Задание 3.2
+    Получить из строки CONFIG список VLAN вида ['10', '20', '30'].
+
+    CONFIG = "switchport trunk allowed vlan 1,3,10,20,30,100"
 '''
 CONFIG = "switchport trunk allowed vlan 1,3,10,20,30,100"
 
